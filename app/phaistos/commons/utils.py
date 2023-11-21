9cd0a5f767505d00d3250b71dd6b3a821027316f
George Stamatakis <george.stamatakis@gmail.com>
2023-11-21 12:24:53 +0000

    Correct gender reference for the recipients section

--- app/phaistos/commons/utils.py
+++ app/phaistos/commons/utils.py
@@ -1,9 +1,20 @@
 from employees.models import Employee
 
 
+<<<<<<< Updated upstream
+=======
+def employee_is_principal():
+    is_principal = False
+    return is_principal
+
+
+def principals_school_unit():
+    school_unit = "Δοκιμαστικό Γυμνάσιο"
+    return school_unit
+>>>>>>> Stashed changes
 
 def employee_is_education_consultant():
-    is_education_consultant = False
+    is_education_consultant = True
     specialization = "ΠΕ99"
     return is_education_consultant, specialization
 
