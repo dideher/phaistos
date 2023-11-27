2d7b8bc787afc0f1668a708f5709a30e9c031ea2
George Stamatakis <george.stamatakis@gmail.com>
2023-11-27 07:52:03 +0000

    Set as default the the employee is not a teaching consultant

--- app/phaistos/commons/utils.py
+++ app/phaistos/commons/utils.py
@@ -1,20 +1,8 @@
 from employees.models import Employee
 
 
-<<<<<<< Updated upstream
-=======
-def employee_is_principal():
-    is_principal = False
-    return is_principal
-
-
-def principals_school_unit():
-    school_unit = "Δοκιμαστικό Γυμνάσιο"
-    return school_unit
->>>>>>> Stashed changes
-
 def employee_is_education_consultant():
-    is_education_consultant = True
+    is_education_consultant = False
     specialization = "ΠΕ99"
     return is_education_consultant, specialization
 
